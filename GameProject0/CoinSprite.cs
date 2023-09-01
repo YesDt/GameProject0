60d81c1ecd3982a2de6dc6f18e5df35479d0e097
Darius Tyson <dariust@ksu.edu>
2023-09-01 14:45:29 -0500

    Implemented animating coins

---
+++ GameProject0/CoinSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameProject0
+{
+    public class CoinSprite
+    {
+        private const float ANIMATION_SPEED = 0.1f;
+
+        public double animationTimer;
+
+        private int _animationFrame;
+
+        private Vector2 _position;
+
+        private Texture2D _texture;
+
+        public CoinSprite(Vector2 position)
+        {
+            this._position = position;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            _texture = content.Load<Texture2D>("Coins");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(animationTimer > ANIMATION_SPEED)
+            {
+                _animationFrame++;
+                if (_animationFrame > 7) _animationFrame = 0;
+                animationTimer -= ANIMATION_SPEED;
+            }
+
+            var source = new Rectangle(_animationFrame * 16, 0, 16, 16);
+            spriteBatch.Draw(_texture, _position, source, Color.White);
+        }
+    }
+}
--- GameProject0/Game1.cs
+++ GameProject0/Game1.cs
@@ -9,7 +9,10 @@ namespace GameProject0
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private CoinSprite[] _coins;
+
         private SpriteFont _title;
+        private SpriteFont _pressEnter;
 
         public Game1()
         {
@@ -21,6 +24,11 @@ namespace GameProject0
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _coins = new CoinSprite[]
+            {
+                new CoinSprite(new Vector2(245, 90)),
+                new CoinSprite(new Vector2(520, 90))
+            };
 
             base.Initialize();
         }
@@ -29,6 +37,8 @@ namespace GameProject0
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _title = Content.Load<SpriteFont>("Title");
+            _pressEnter = Content.Load<SpriteFont>("PressEnter");
+            foreach (var coin in _coins) coin.LoadContent(Content);
             // TODO: use this.Content to load your game content here
         }
 
@@ -39,17 +49,21 @@ namespace GameProject0
 
             // TODO: Add your update logic here
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter)) Exit();
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.DeepSkyBlue);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.DrawString(_title, "GOLD RUSH", new Vector2(260, 70), Color.Gold);
+            _spriteBatch.DrawString(_title, "GOLD RUSH", new Vector2(270, 70), Color.Gold);
+            _spriteBatch.DrawString(_pressEnter, "Press Enter to exit the game", new Vector2(260, 120), Color.Red);
+            foreach (var coin in _coins) coin.Draw(gameTime, _spriteBatch);
 
             _spriteBatch.End();
 
