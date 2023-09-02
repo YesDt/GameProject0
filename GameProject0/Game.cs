03b3791d2f220da0d423c4819125d9dff784c233
Darius Tyson <dariust@ksu.edu>
2023-09-01 20:03:25 -0500

    Game Project 0 Complete

--- GameProject0/Game.cs
+++ GameProject0/Game.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace GameProject0
 {
-    public class Game1 : Game
+    public class Game : Microsoft.Xna.Framework.Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -14,11 +14,12 @@ namespace GameProject0
         private Texture2D _crown;
         private Texture2D _bars;
         private Texture2D _chest;
+        private Texture2D _titleScreen;
 
         private SpriteFont _title;
         private SpriteFont _pressEnter;
 
-        public Game1()
+        public Game()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -44,6 +45,7 @@ namespace GameProject0
             _crown = Content.Load<Texture2D>("gold");
             _bars = Content.Load<Texture2D>("gold");
             _chest = Content.Load<Texture2D>("gold");
+            _titleScreen = Content.Load<Texture2D>("titleScreen");
             _pressEnter = Content.Load<SpriteFont>("PressEnter");
             foreach (var coin in _coins) coin.LoadContent(Content);
             // TODO: use this.Content to load your game content here
@@ -67,13 +69,13 @@ namespace GameProject0
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-
+            _spriteBatch.Draw(_titleScreen, new Vector2(0, 0), Color.White);
             _spriteBatch.DrawString(_title, "GOLD RUSH", new Vector2(270, 70), Color.Gold);
             _spriteBatch.Draw(_crown, new Vector2(460, 80), new Rectangle(0, 96, 32, 32), Color.White, .75f, new Vector2(16, 16), 1.0f, SpriteEffects.None, 0);
-            _spriteBatch.DrawString(_pressEnter, "Press Enter to exit the game", new Vector2(260, 120), Color.Red);
+            _spriteBatch.DrawString(_pressEnter, "Press Enter to exit the game", new Vector2(260, 120), Color.Black);
             foreach (var coin in _coins) coin.Draw(gameTime, _spriteBatch);
-            _spriteBatch.Draw(_bars, new Vector2(120, 300), new Rectangle(64, 64, 32, 32), Color.White, 0f, new Vector2(16, 16), 2.0f, SpriteEffects.None, 0);
-            _spriteBatch.Draw(_chest, new Vector2(600, 300), new Rectangle(96, 64, 32, 32), Color.White, 0f, new Vector2(16, 16), 3.0f, SpriteEffects.None, 0);
+            _spriteBatch.Draw(_bars, new Vector2(50, 160), new Rectangle(64, 64, 32, 32), Color.White, 0f, new Vector2(16, 16), 2.0f, SpriteEffects.None, 0);
+            _spriteBatch.Draw(_chest, new Vector2(700, 260), new Rectangle(96, 64, 32, 32), Color.White, 0f, new Vector2(16, 16), 5.0f, SpriteEffects.None, 0);
             _spriteBatch.End();
 
             base.Draw(gameTime);
